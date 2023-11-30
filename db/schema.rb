9edf64357692d9155667c1e16d183b80747fdbc9
Josh Villanueva <77131654+JoshVL@users.noreply.github.com>
2023-11-30 17:08:52 -0600

    update schema and seeds

--- db/schema.rb
+++ db/schema.rb
@@ -2,8 +2,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_30_214704) do
   create_table "users", force: :cascade do |t|
     t.string "login"
     t.string "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
